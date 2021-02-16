716a29d79fe32666beccaa75a257c75fb8288c7f
Donddu <farierd11@gmail.com>
2021-02-16 05:54:59 +0200

    asd
    asd

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -22,6 +22,6 @@ public class PlayerMovement : MonoBehaviour
 
     void Movement()
     {
-        rb.velocity = new Vector2(playerInput.movement.x, rb.velocity.y);
+        rb.velocity = new Vector2(playerInput.movement.x*10, rb.velocity.y);
     }
 }
