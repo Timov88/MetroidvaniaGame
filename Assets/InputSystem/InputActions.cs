45014f2b1d79457dd792ab239f2663f2529444e1
Donddu <farierd11@gmail.com>
2021-02-16 03:45:54 +0200

    ASD
    ASD

--- Assets/InputSystem/InputActions.cs
+++ Assets/InputSystem/InputActions.cs
@@ -28,11 +28,27 @@ public class @InputActions : IInputActionCollection, IDisposable
                 },
                 {
                     ""name"": ""Jump"",
-                    ""type"": ""Button"",
+                    ""type"": ""Value"",
                     ""id"": ""51b52acd-7024-46a3-b756-ea4d797a3300"",
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
-                    ""interactions"": """"
+                    ""interactions"": ""Press""
+                },
+                {
+                    ""name"": ""Nappi"",
+                    ""type"": ""Value"",
+                    ""id"": ""b56941d8-bb1d-4bc6-b9bf-b9532e5001bc"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": ""Press""
+                },
+                {
+                    ""name"": ""Liikkumine"",
+                    ""type"": ""Button"",
+                    ""id"": ""7f77bcc4-604b-4822-9093-b45eada790ed"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": ""Press""
                 }
             ],
             ""bindings"": [
@@ -51,7 +67,7 @@ public class @InputActions : IInputActionCollection, IDisposable
                     ""name"": """",
                     ""id"": ""94eaf6b2-436d-49a5-8020-0c75f59429c5"",
                     ""path"": ""<Keyboard>/space"",
-                    ""interactions"": ""Press"",
+                    ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
                     ""action"": ""Jump"",
@@ -61,11 +77,93 @@ public class @InputActions : IInputActionCollection, IDisposable
                 {
                     ""name"": """",
                     ""id"": ""7fd35a9b-df47-4989-b980-203f655be150"",
-                    ""path"": ""<Gamepad>/buttonSouth"",
+                    ""path"": ""<Gamepad>/rightTrigger"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
                     ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""6ff2e2f5-02b3-4ffe-a696-07ac58ae7a67"",
+                    ""path"": ""<Gamepad>/leftStick/left"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""8f862775-d977-4ca7-8380-89dbdd57531d"",
+                    ""path"": ""<Gamepad>/rightTrigger"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Nappi"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""4577e65b-e04e-4469-a5c0-10468887919f"",
+                    ""path"": ""<Gamepad>/leftStick/left"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Liikkumine"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""c1809137-be38-4757-a4c6-18a7f8af377b"",
+                    ""path"": ""<Gamepad>/leftStick/down"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Liikkumine"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        },
+        {
+            ""name"": ""Player1"",
+            ""id"": ""67e22c6e-ef03-443f-b30a-fdad84078c57"",
+            ""actions"": [
+                {
+                    ""name"": ""New action"",
+                    ""type"": ""Button"",
+                    ""id"": ""e73af631-8a85-434a-9db1-69c928124c43"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""eb840975-2f7f-48ea-8efd-dd4fe2ea86f0"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""New action"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""8bf7cf66-bd07-42b8-baed-90117ece4dd8"",
+                    ""path"": ""<Gamepad>/buttonSouth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""New action"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -78,6 +176,11 @@ public class @InputActions : IInputActionCollection, IDisposable
         m_Inputs = asset.FindActionMap("Inputs", throwIfNotFound: true);
         m_Inputs_Axis = m_Inputs.FindAction("Axis", throwIfNotFound: true);
         m_Inputs_Jump = m_Inputs.FindAction("Jump", throwIfNotFound: true);
+        m_Inputs_Nappi = m_Inputs.FindAction("Nappi", throwIfNotFound: true);
+        m_Inputs_Liikkumine = m_Inputs.FindAction("Liikkumine", throwIfNotFound: true);
+        // Player1
+        m_Player1 = asset.FindActionMap("Player1", throwIfNotFound: true);
+        m_Player1_Newaction = m_Player1.FindAction("New action", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -129,12 +232,16 @@ public class @InputActions : IInputActionCollection, IDisposable
     private IInputsActions m_InputsActionsCallbackInterface;
     private readonly InputAction m_Inputs_Axis;
     private readonly InputAction m_Inputs_Jump;
+    private readonly InputAction m_Inputs_Nappi;
+    private readonly InputAction m_Inputs_Liikkumine;
     public struct InputsActions
     {
         private @InputActions m_Wrapper;
         public InputsActions(@InputActions wrapper) { m_Wrapper = wrapper; }
         public InputAction @Axis => m_Wrapper.m_Inputs_Axis;
         public InputAction @Jump => m_Wrapper.m_Inputs_Jump;
+        public InputAction @Nappi => m_Wrapper.m_Inputs_Nappi;
+        public InputAction @Liikkumine => m_Wrapper.m_Inputs_Liikkumine;
         public InputActionMap Get() { return m_Wrapper.m_Inputs; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -150,6 +257,12 @@ public class @InputActions : IInputActionCollection, IDisposable
                 @Jump.started -= m_Wrapper.m_InputsActionsCallbackInterface.OnJump;
                 @Jump.performed -= m_Wrapper.m_InputsActionsCallbackInterface.OnJump;
                 @Jump.canceled -= m_Wrapper.m_InputsActionsCallbackInterface.OnJump;
+                @Nappi.started -= m_Wrapper.m_InputsActionsCallbackInterface.OnNappi;
+                @Nappi.performed -= m_Wrapper.m_InputsActionsCallbackInterface.OnNappi;
+                @Nappi.canceled -= m_Wrapper.m_InputsActionsCallbackInterface.OnNappi;
+                @Liikkumine.started -= m_Wrapper.m_InputsActionsCallbackInterface.OnLiikkumine;
+                @Liikkumine.performed -= m_Wrapper.m_InputsActionsCallbackInterface.OnLiikkumine;
+                @Liikkumine.canceled -= m_Wrapper.m_InputsActionsCallbackInterface.OnLiikkumine;
             }
             m_Wrapper.m_InputsActionsCallbackInterface = instance;
             if (instance != null)
@@ -160,13 +273,58 @@ public class @InputActions : IInputActionCollection, IDisposable
                 @Jump.started += instance.OnJump;
                 @Jump.performed += instance.OnJump;
                 @Jump.canceled += instance.OnJump;
+                @Nappi.started += instance.OnNappi;
+                @Nappi.performed += instance.OnNappi;
+                @Nappi.canceled += instance.OnNappi;
+                @Liikkumine.started += instance.OnLiikkumine;
+                @Liikkumine.performed += instance.OnLiikkumine;
+                @Liikkumine.canceled += instance.OnLiikkumine;
             }
         }
     }
     public InputsActions @Inputs => new InputsActions(this);
+
+    // Player1
+    private readonly InputActionMap m_Player1;
+    private IPlayer1Actions m_Player1ActionsCallbackInterface;
+    private readonly InputAction m_Player1_Newaction;
+    public struct Player1Actions
+    {
+        private @InputActions m_Wrapper;
+        public Player1Actions(@InputActions wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Newaction => m_Wrapper.m_Player1_Newaction;
+        public InputActionMap Get() { return m_Wrapper.m_Player1; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(Player1Actions set) { return set.Get(); }
+        public void SetCallbacks(IPlayer1Actions instance)
+        {
+            if (m_Wrapper.m_Player1ActionsCallbackInterface != null)
+            {
+                @Newaction.started -= m_Wrapper.m_Player1ActionsCallbackInterface.OnNewaction;
+                @Newaction.performed -= m_Wrapper.m_Player1ActionsCallbackInterface.OnNewaction;
+                @Newaction.canceled -= m_Wrapper.m_Player1ActionsCallbackInterface.OnNewaction;
+            }
+            m_Wrapper.m_Player1ActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Newaction.started += instance.OnNewaction;
+                @Newaction.performed += instance.OnNewaction;
+                @Newaction.canceled += instance.OnNewaction;
+            }
+        }
+    }
+    public Player1Actions @Player1 => new Player1Actions(this);
     public interface IInputsActions
     {
         void OnAxis(InputAction.CallbackContext context);
         void OnJump(InputAction.CallbackContext context);
+        void OnNappi(InputAction.CallbackContext context);
+        void OnLiikkumine(InputAction.CallbackContext context);
+    }
+    public interface IPlayer1Actions
+    {
+        void OnNewaction(InputAction.CallbackContext context);
     }
 }
--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -2,18 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerInput : MonoBehaviour
 {
-    PlayerMove playerMove;
-    InputActions inputActions;
+    //PlayerMove playerMove;
+    //InputActions inputActions;
+    private InputActions controls;
+    private float triggerValue;
+
     void Awake()
     {
-        Debug.Log("AWAKE TOIMII");
-        inputActions = new InputActions();
-        playerMove = GetComponent<PlayerMove>();
+        //inputActions = new InputActions();
+        //playerMove = GetComponent<PlayerMove>();
+        controls = new InputActions();
         
-        inputActions.Inputs.Jump.performed += ctx => Jump();
+        //controls.Inputs.Jump.performed += ctx => Jump();
         //inputActions.Inputs.Jump.canceled += ctx => JumpCanceled();
+
+        controls.Inputs.Jump.performed += context => triggerValue = context.ReadValue<float>();
+        controls.Inputs.Jump.canceled += context => triggerValue = 0f;
+        controls.Inputs.Jump.started += context => Jump();
+        controls.Inputs.Jump.canceled += context => JumpOff();
 
     }
 
@@ -26,16 +35,31 @@ public class PlayerInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 axisInput = inputActions.Inputs.Axis.ReadValue<Vector2>();
+        //Debug.Log("Kulli Value: " + triggerValue);
+        Vector2 axisInput = controls.Inputs.Axis.ReadValue<Vector2>();
         Vector2 movement = new Vector2 { x = axisInput.x, y = axisInput.y };
         movement.Normalize();
+        Debug.Log("ISO" + axisInput);
+        Debug.Log(movement);
     }
 
-    void Jump()
+    private void Jump()
     {
-        Debug.Log("asdasd");
-        playerMove.Jump();
+        Debug.Log("Kulli ON");
+        //playerMove.Jump();
     }
 
+    private void JumpOff()
+    {
+        Debug.Log("Kulli OFF");
+    }
+
+    void OnEnable() {
+        controls.Enable();
+    }
+
+     void OnDisable() {
+        controls.Disable();
+    }
 
 }
