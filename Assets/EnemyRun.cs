a4ad34393710192d9702d83a470cf03b1ed7c069
timokotona <timo.visuri88@gmail.com>
2021-02-17 00:09:33 +0200

    sad

--- Assets/EnemyRun.cs
+++ Assets/EnemyRun.cs
@@ -7,13 +7,13 @@ public class EnemyRun : StateMachineBehaviour
     Transform player;
     Rigidbody2D rb;
     Enemy enemy;
-    float attackRange;
+    float attackRange = 2f;
     //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        rb = animator.GetComponent<Rigidbody2D>();
-        enemy = animator.GetComponent<Enemy>();
+        rb = animator.GetComponentInParent<Rigidbody2D>();
+        enemy = animator.GetComponentInParent<Enemy>();
     }
 
     //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
