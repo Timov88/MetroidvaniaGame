a5d98097de89c88d51782895071213638183774a
timokotona <timo.visuri88@gmail.com>
2021-02-16 19:40:23 +0200

    kamera seuraa pelaajaa ja vihollinen

--- Assets/EnemyRun.cs
+++ Assets/EnemyRun.cs
@@ -6,16 +6,19 @@ public class EnemyRun : StateMachineBehaviour
 {
     Transform player;
     Rigidbody2D rb;
+    Enemy enemy;
     //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         rb = animator.GetComponent<Rigidbody2D>();
+        enemy = animator.GetComponent<Enemy>();
     }
 
     //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        enemy.Flip();
         Vector2 target = new Vector2(player.position.x, rb.position.y);
         Vector2 newPos = Vector2.MoveTowards(rb.position, target, 3 * Time.fixedDeltaTime);
         rb.MovePosition(newPos);
--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -6,11 +6,22 @@ public class Enemy : MonoBehaviour
 {
     Rigidbody2D rb;
     Transform player;
+    SpriteRenderer sr;
+    bool facingRight = false;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         player = GameObject.Find("Player").transform;
+    }
+
+    public void Flip()
+    {
+        if (player.position.x > transform.position.x && !facingRight || player.position.x < transform.position.x && facingRight)
+        {
+            facingRight = !facingRight;
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        }    
     }
 
     // Update is called once per frame
