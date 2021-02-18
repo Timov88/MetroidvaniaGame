518681e8ceb1aaf487b19ffb8d4e7e7fe5332777
timokotona <timo.visuri88@gmail.com>
2021-02-18 20:07:09 +0200

    asd

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -24,7 +24,7 @@ public class PlayerController : MonoBehaviour
     
 
     void Start() 
-        {
+    {
             rb = GetComponent<Rigidbody2D>();
             boxCollider2D = GetComponent<BoxCollider2D>();
             anim = GetComponent<Animator>();
@@ -32,68 +32,69 @@ public class PlayerController : MonoBehaviour
             audio = GetComponentInChildren<AudioController>();
     }
 
-        void FixedUpdate() 
+    void FixedUpdate() 
+    {
+        IsGrounded();
+        if (!dashing)
         {
-            IsGrounded();
-            if (!dashing)
-            {
-                rb.velocity = new Vector2(horizontal*movementSpeed, rb.velocity.y);
-            }
-            //If you change fixed update use Time.deltaTime so your movement speed is not gonna get cucked
-            //rb.velocity = new Vector2(horizontal*movementSpeed*(Time.deltaTime*100), rb.velocity.y);
+            rb.velocity = new Vector2(horizontal*movementSpeed, rb.velocity.y);
             anim.SetFloat("Run", Mathf.Abs(horizontal));
             anim.speed = Mathf.Abs(horizontal);
         }
+        //If you change fixed update use Time.deltaTime so your movement speed is not gonna get cucked
+        //rb.velocity = new Vector2(horizontal*movementSpeed*(Time.deltaTime*100), rb.velocity.y);
+            
+    }
 
        
-        public void OnMoveInput(float horizontal, float vertical)
-        {
-            this.horizontal = horizontal;
-            this.vertical = vertical;
+    public void OnMoveInput(float horizontal, float vertical)
+    {
+        this.horizontal = horizontal;
+        this.vertical = vertical;
 
-           if(horizontal < 0 && !facingRight || horizontal > 0 && facingRight)
-           {
-                transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
-                facingRight = !facingRight;
-           }
-            //Debug.Log($"{horizontal},{vertical}");
-        }
-
-        public void OnJumpInput(bool jumpInput)
+        if(horizontal < 0 && !facingRight || horizontal > 0 && facingRight)
         {
-            if (IsGrounded())
-            {
-                audio.PlayjumpSound();
-                Debug.Log("SOITA AUDIO");
-                //jumpAudio.Play();//
-                rb.AddForce(Vector2.up*jumpSpeed, ForceMode2D.Impulse);
-            }
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+            facingRight = !facingRight;
         }
+        //Debug.Log($"{horizontal},{vertical}");
+    }
 
-        public void OnDashInput(bool dashInput)
+    public void OnJumpInput(bool jumpInput)
+    {
+        if (IsGrounded())
         {
-            dash = Dash();
-            StartCoroutine(dash);
+            audio.PlayjumpSound();
+            Debug.Log("SOITA AUDIO");
+            //jumpAudio.Play();//
+            rb.AddForce(Vector2.up*jumpSpeed, ForceMode2D.Impulse);
         }
+    }
 
-        public void OnShootInput(bool shootInput)
-        {
-            Instantiate(bullet, new Vector2(1,1), Quaternion.identity);
-        }
+    public void OnDashInput(bool dashInput)
+    {
+        dash = Dash();
+        StartCoroutine(dash);
+    }
 
-        private bool IsGrounded() 
-        {
-            RaycastHit2D hit = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down, 0.05f, platformLayerMask);
-            return hit.collider != null;
-        }
+    public void OnShootInput(bool shootInput)
+    {
+        Instantiate(bullet, new Vector2(1,1), Quaternion.identity);
+    }
 
-        IEnumerator Dash()
-        {
-            dashing = true;
-            rb.velocity = new Vector2(transform.localScale.x*dashSpeed, rb.velocity.y);
-            dashTrail.InvokeRepeating("SpawnTrailPart",0,0.03f);
-            yield return new WaitForSeconds(dashTime);
-            dashTrail.CancelInvoke("SpawnTrailPart");
-            dashing = false;           
-        }
+    private bool IsGrounded() 
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down, 0.05f, platformLayerMask);
+        return hit.collider != null;
+    }
+
+    IEnumerator Dash()
+    {
+        dashing = true;
+        rb.velocity = new Vector2(transform.localScale.x*dashSpeed, rb.velocity.y);
+        dashTrail.InvokeRepeating("SpawnTrailPart",0,0.03f);
+        yield return new WaitForSeconds(dashTime);
+        dashTrail.CancelInvoke("SpawnTrailPart");
+        dashing = false;           
+    }
 }
