2012dccf8f638e8c78f753b97768dc85e503feee
timokotona <timo.visuri88@gmail.com>
2021-02-18 12:27:33 +0200

    qfd

---
+++ Assets/Scripts/Player/DashTrail.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashTrail : MonoBehaviour
+{
+    List<GameObject> trailParts = new List<GameObject>();
+    void SpawnTrailPart()
+    {
+        GameObject trailPart = new GameObject();
+        SpriteRenderer trailPartRenderer = trailPart.AddComponent<SpriteRenderer>();
+        trailPartRenderer.sprite = GetComponent<SpriteRenderer>().sprite;
+        trailPart.transform.position = transform.position;
+        trailPart.transform.localScale = transform.parent.transform.localScale;
+        trailParts.Add(trailPart);
+        
+
+        StartCoroutine(FadeTrailPart(trailPartRenderer));
+        Destroy(trailPart, 0.08f); // replace 0.5f with needed lifeTime
+    }
+
+    IEnumerator FadeTrailPart(SpriteRenderer trailPartRenderer)
+    {
+        
+        Color color = trailPartRenderer.color;
+        color.a -= 0.5f;
+        color.r -= 0.3f;
+        color.g -= 0.3f;// replace 0.5f with needed alpha decrement
+        trailPartRenderer.color = color;
+
+        yield return new WaitForEndOfFrame();
+        
+    }
+    IEnumerator DestroyTrailPart(GameObject trailPart, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        trailParts.Remove(trailPart);
+        Destroy(trailPart);
+    }
+    public void FlipTrail()
+    {
+        if(trailParts.Count != 0)
+        {
+            foreach (GameObject trailPart in trailParts)
+            {
+                if(trailPart != null)
+                {
+                    Vector3 trailPartLocalScale = trailPart.transform.localScale;
+                    trailPartLocalScale.x = transform.parent.localScale.x;
+                    trailPart.transform.localScale = transform.parent.localScale;
+                }
+                
+            }
+        }
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
