ec39fc7f6e37c614b829d9cc55f10b3b361c4d06
Turismo1 <sound.of.turismo@gmail.com>
2021-02-26 01:10:40 +0200

    nyt alko skulaa

--- Assets/EnemyRun.cs
+++ Assets/EnemyRun.cs
@@ -8,8 +8,8 @@ public class EnemyRun : StateMachineBehaviour
     Rigidbody2D rb;
     Enemy enemy;
     float attackRange = 1.4f;
-    //HitPoints death;
-    
+    HitPoints death;
+    bool playerDeath;
 
 
 
@@ -21,13 +21,13 @@ public class EnemyRun : StateMachineBehaviour
         player = GameObject.FindGameObjectWithTag("Player").transform;
         rb = animator.GetComponentInParent<Rigidbody2D>();
         enemy = animator.GetComponentInParent<Enemy>();
-        //bool playerDeath = GameObject.Find("playerDeath").GetComponentInParent<HitPoints>();
+        death = player.GetComponent<HitPoints>();
     }
 
     //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
+        
         enemy.LookAtPlayer();
         Vector2 target = new Vector2(player.position.x, rb.position.y);
         Vector2 newPos = Vector2.MoveTowards(rb.position, target, 2 * Time.fixedDeltaTime);
@@ -36,18 +36,18 @@ public class EnemyRun : StateMachineBehaviour
             rb.MovePosition(newPos);
         }
 
-        if (Vector2.Distance(player.position, rb.position) <= attackRange)
+        if (Vector2.Distance(player.position, rb.position) <= attackRange )
         {
             
             animator.SetTrigger("Attack");
             enemy.Attack(true);
         }
 
-        //if (death.playerDeath == true)
-        //{
-        //    animator.ResetTrigger("Attack");
-        //    enemy.Attack(false);
-        //}
+        if (death.playerDeath == true)
+        {
+            animator.ResetTrigger("Attack");
+            enemy.Attack(false);
+        }
     }
 
 
